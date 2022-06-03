73d831ca1ab094d7ff4bcd9d27ba3ae7c04dad74
macbook <macbook@Brian-2.local>
2022-06-03 08:59:50 +0300

    Created the function collectKeywordsFromMonthlySheets to collect data from other sheets apart from the main sheet

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 function getSheetNames() {
-    var sheetArray = new Array();
-    var sheets = SpreadsheetApp.getActiveSpreadsheet().getSheets();
-  
-    for (var i=0; i<sheets.length; i++) {
-      sheetArray.push(sheets[i].getName());
-    }
-  
-    return sheetArray;
+  var sheetArray = new Array();
+  var sheets = SpreadsheetApp.getActiveSpreadsheet().getSheets();
+
+  for (var i = 0; i < sheets.length; i++) {
+    sheetArray.push(sheets[i].getName());
   }
+
+  return sheetArray;
+}
+
+function collectKeywordsFromMonthlySheets() {
+  var keywordList = new Array();
+  var sheetNames = getSheetNames();
+
+  for (var i = 0; i < sheetNames.length; i++) {
+    if (sheetNames[i] === "Keyword Tracking Sheet") {
+      sheetNames.splice(i, 1);
+    }
+  }
+
+  for (var i = 0; i < sheetNames.length; i++) {
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(
+      sheetNames[i]
+    );
+    keywordList.push(sheet.getRange("A2:A").getValues());
+  }
+
+  allKeywords = [].concat.apply([], keywordList);
+
+  return allKeywords;
+}
